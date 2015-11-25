fb6cad2e1ae22881a9d2d36562537bb8b3a25835
Ru Chern <iruchern@gmail.com>
2015-11-25 16:31:57 +0800

    Added more codes.

--- Assignment 1/assignment1.py
+++ Assignment 1/assignment1.py
@@ -1,1 +1,22 @@
-data = load_data("animal_heights.csv")
+import csv
+import math
+
+def load_data(filename):
+    file = open(filename)
+    reader = csv.reader(file)
+    heights = []
+
+    for animal in reader:
+        name = animal.pop(0)
+        animalArray = []
+
+        for height in animal:
+            animalArray.append(float(height))
+
+            tup = (name, animalArray)
+            heights.append(tup)
+
+        print heights
+        return heights
+
+load_data("animal_heights.csv")
